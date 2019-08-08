41288fa56e0b76d225d50e1f807dc490913dea7e
Robert Claus <robertclaus@gmail.com>
2019-08-07 21:51:06 -0500

    Try headers again

--- ifttt/views.py
+++ ifttt/views.py
@@ -7,8 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 def is_valid(request):
     key = settings.IFTTT_CHANNEL_KEY
     print(f"Headers: {request.META}")
-    channel_key = request.META.get("IFTTT-Channel-Key")
-    service_key = request.META.get("IFTTT-Service-Key")
+    channel_key = request.META.get("HTTP_IFTTT_CHANNEL_KEY")
+    service_key = request.META.get("HTTP_IFTTT_SERVICE_KEY")
 
     if not channel_key or not service_key or key != channel_key or key != service_key:
         return False
@@ -38,6 +38,25 @@ def update(request):
     action_fields = contents.get('actionFields')
 
     if not contents or not action_fields or not action_fields.get('key') or not action_fields.get('code'):
+        return JsonResponse({"errors": [{"message": "Missing Field."}]}, status=400)
+
+    response_contents = {"ok": "True"}
+    return JsonResponse(response_contents)
+
+
+@csrf_exempt
+def state(request):
+    if not is_valid(request):
+        return invalid_response()
+
+    print(f"Request GET: {request.GET}")
+    print(f"Request POST: {request.POST}")
+    print(f"Request Body: {request.body}")
+
+    contents = json.loads(request.body.decode('utf-8'))
+    trigger_fields = contents.get('triggerFields')
+
+    if not contents or not trigger_fields or not trigger_fields.get('key') or not trigger_fields.get('code'):
         return JsonResponse({"errors": [{"message": "Missing Field."}]}, status=400)
 
     response_contents = {"ok": "True"}
