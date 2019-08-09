9a2dbb83859d1087c7e4b3b7ae36bc895f3b04c2
Robert Claus <robertclaus@gmail.com>
2019-08-08 21:04:00 -0500

    Fix UTF-8

--- ifttt/admin.py
+++ ifttt/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from ifttt.models import Clause, Event
+
+
+class ClauseAdmin(admin.ModelAdmin):
+    pass
+
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Clause, ClauseAdmin)
+admin.site.register(Event, EventAdmin)
